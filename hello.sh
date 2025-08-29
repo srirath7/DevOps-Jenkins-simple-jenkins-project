69332498959db56d7762b0ca51470a390d5b8783
srirathreddy <srirathreddy@gmail.com>
2025-08-29 10:23:53 +0530

    initial commit2

--- hello.sh
+++ hello.sh
@@ -1,4 +1,26 @@
 
 #!/bin/bash
+chmod +x hello.sh
 echo "Hello from Jenkins!"
 echo "This is a test build from a Freestyle job."
+
+git status
+sleep 2
+
+echo "$(date)"
+sleep 3
+
+uname -a
+sleep 2
+
+ls -lah
+sleep3
+
+mkdir -p build
+cat build/build_log.txt
+
+sleep 5
+
+echo "స్థితప్రజ్ఞుడు:A sthithaprajnudu is someone who lives with inner peace, unshaken by ups and downs of life" >sthitahprajnudu.txt
+
+cat sthitahprajnudu.txt
